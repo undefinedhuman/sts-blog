67ac463017ef2b7be419ede8cd6d8d97c09df686
Alexander Padberg <alexander.padberg@code.berlin>
2020-04-20 20:12:41 +0200

    docs(changelog): add git commit for new version and chagelog file appending

--- changelog.js
+++ changelog.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const repo_link = "https://github.com/undefinedhuman/sts-blog/"
 
 const commitLog = child_process
-    .execSync(`git log --reverse --pretty=format:%H---SPLIT---%h---SPLIT---%s---COMMIT---`)
+    .execSync(`git log ${child_process.execSync('git describe --tags').toString('utf-8').split('-')[0]}...HEAD --reverse --pretty=format:%H---SPLIT---%h---SPLIT---%s---COMMIT---`)
     .toString('utf-8')
     .split("---COMMIT---")
     .map(commit => {
@@ -18,7 +18,7 @@ const currentChangelog = fs.readFileSync("./CHANGELOG.md", "utf-8");
 
 let commitTypes = [
     { name: "Added",            types: [ "feat", "test" ],                      command: "npm version --no-git-tag-version minor",  commits: [] },
-    { name: "Changed",          types: [ "style", "refactor", "build", "ci" ],  command: "npm version --no-git-tag-version minor",                                        commits: [] },
+    { name: "Changed",          types: [ "style", "refactor", "build", "ci" ],  command: "",                                        commits: [] },
     { name: "Fixed",            types: [ "fix" ],                               command: "npm version --no-git-tag-version patch",  commits: [] },
     { name: "Removed",          types: [ "revert" ],                            command: "",                                        commits: [] },
     { name: "Performance",      types: [ "perf" ],                              command: "",                                        commits: [] },
@@ -36,8 +36,43 @@ commitLog.forEach(commit => {
     })
 });
 
-commitTypes.forEach(type => {
-    if(type.commits.length === 0) return;
-    console.log(type.name + "\n")
-    console.log(type.commits)
+const version = child_process.execSync("npm --loglevel silent run version");
+
+child_process.execSync(`git add .`)
+child_process.execSync(`git commit -m "chore(version): Bump to ${version}"`)
+child_process.execSync(`git tag -am "Tag for ${version}" ${version}`)
+
+const versionCommit = child_process
+    .execSync(`git log HEAD~1...HEAD --pretty=format:%H---SPLIT---%h---SPLIT---%s---COMMIT---`)
+    .toString('utf-8')
+    .split("---COMMIT---")
+    .map(commit => {
+        commit = commit.replace("\n", "");
+        const [sha, hash, message] = commit.split("---SPLIT---")
+        return { sha, hash, message }
+    })
+    .filter(commit => Boolean(commit.sha))[0];
+
+commitTypes[5].commits.push(`- Bump to ${version} ([${versionCommit.hash}](${repo_link}/commit/${versionCommit.sha}))\n`)
+
+let options = {
+    weekday: 'long', year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit'
+};
+
+let changelog = `# Version ${version} (${
+    new Date().toLocaleString('en-us', options)
+})\n\n`;
+
+commitTypes.forEach(commitType => {
+    changelog += `## ${commitType.name}\n`
+    commitType.commits.forEach(commit => {
+        changelog += commit
+    })
+    changelog += "\n"
 })
+
+fs.writeFileSync("./CHANGELOG.md", `${changelog}${currentChangelog}`);
+
+child_process.execSync("git add .")
+child_process.execSync("git commit --amend")
+child_process.execSync(`git push && git push --tags`)
