38a3b8a081727f8dc798f54cada66054d605213b
Travis CI <travis@travis-ci.org>
2020-04-21 18:20:31 +0200

    feat(endpoints): add basic CRUD endpoints

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,21 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.json('Basic information send as json!'))
+app.get('/', function (req, res) {
+    res.json('Basic get request, response in json!')
+})
+
+app.post('/', function (req, res) {
+    res.json('Basic post request, response in json!')
+})
+
+app.put('/put', function (req, res) {
+    res.send('Basic put request, response in json!')
+})
+
+app.delete('/delete', function (req, res) {
+    res.send('Basic delete request, response in json!')
+})
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
