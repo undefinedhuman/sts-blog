9dd6478fce0152155fc5d0e4767b70b272d8bd84
Travis CI <travis@travis-ci.org>
2020-04-22 13:22:10 +0200

    feat(frontend): add basic routing and first static site

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 const express = require("express")
+const path = require('path')
+
 const app = express()
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.json('Basic get request, response in json!')
@@ -9,12 +13,12 @@ app.post('/', function (req, res) {
     res.json('Basic post request, response in json!')
 })
 
-app.put('/put', function (req, res) {
-    res.send('Basic put request, response in json!')
+app.put('/entry', function (req, res) {
+    res.json('Basic put request, response in json!')
 })
 
-app.delete('/delete', function (req, res) {
-    res.send('Basic delete request, response in json!')
+app.delete('/entry', function (req, res) {
+    res.json('Basic delete request, response in json!')
 })
 
 module.exports = app
