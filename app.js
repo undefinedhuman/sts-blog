4e05c874c4df0bd2253d5b73e1bcb1112c4a8e54
Alexander Padberg <alexander.padberg@code.berlin>
2020-04-23 13:08:40 +0200

    feat(database): add connection to local database

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const app = require("./src/server")
+const app = require("./src/app")
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
---
+++ src/app.js
@@ -0,0 +1,42 @@
+const path = require("path")
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+mongoose
+    .connect('mongodb://localhost',)
+    .then(() => { console.log('Connected successfully!'); })
+    .catch((err) => { throw err })
+
+app.use(express.static(path.join(__dirname, "../public")))
+
+app.use(bodyParser.urlencoded({ extended: true }))#
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.json('Basic get request, response in json!')
+})
+
+app.post('/', (req, res) => {
+    res.json('Basic post request, response in json!')
+})
+
+app.put('/entry', (req, res) => {
+    res.json('Basic put request, response in json!')
+})
+
+app.delete('/entry', (req, res) => {
+    res.json('Basic delete request, response in json!')
+})
+
+app.use((req, res, next) => {
+    res.status(404).send(`Sorry can't find ${res.originalUrl}!`)
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send(`Error: ${err}`);
+});
+
+module.exports = app
