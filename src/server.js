7ee1ab34511b91808efbfabeb41a559978a23688
Alexander Padberg <alexander.padberg@code.berlin>
2020-04-29 17:16:08 +0200

    feat(article): add basic CRUD controller for articles

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-const path = require("path")
-const express = require("express")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
 mongoose
     .connect('mongodb://localhost/sts_blog', {
@@ -13,32 +13,32 @@ mongoose
     .then(
         err => { throw err },
         () => { console.log('Connected successfully!') }
-    )
+    );
 
-app.use(express.static(path.join(__dirname, "../public")))
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.json('Basic get request, response in json!')
-})
+});
 
 app.post('/', (req, res) => {
     res.json('Basic post request, response in json!')
-})
+});
 
 app.put('/entry', (req, res) => {
     res.json('Basic put request, response in json!')
-})
+});
 
 app.delete('/entry', (req, res) => {
     res.json('Basic delete request, response in json!')
-})
+});
 
 app.use((req, res, next) => {
     res.status(404).send(`Sorry can't find ${res.originalUrl}!`)
-})
+});
 
 app.use((err, req, res, next) => {
     res.status(500).send(`Error: ${err}`);
